803158f80842874140f6a381c42559894d35ccdf
Score_Under <seejay.11@gmail.com>
2024-10-22 18:48:24 +0100

    Implement some more functions

--- IllogicApps.Simulator/BuiltinFunctions.fs
+++ IllogicApps.Simulator/BuiltinFunctions.fs
@@ -4,6 +4,7 @@ open System.Text.Json
 open System.Text.Json.Nodes
 open IllogicApps.Core
 open Helpers
+open System.Collections.Generic
 
 type LanguageFunction = SimulatorContext -> JsonNode list -> JsonNode
 type Args = JsonNode list
@@ -25,6 +26,14 @@ let toBase64 (str: string) =
 
 let fromBase64 (str: string) =
     str |> System.Convert.FromBase64String |> System.Text.Encoding.UTF8.GetString
+
+let toBinary (str: string) : JsonNode =
+    let base64 = str |> toBase64 in
+
+    new JsonObject(
+        [ new KeyValuePair<string, JsonNode>("$content-type", JsonValue.Create("application/octet-stream"))
+          new KeyValuePair<string, JsonNode>("$content", JsonValue.Create(base64)) ]
+    )
 
 let objectToString (node: JsonNode) : string =
     match node.GetValueKind() with
@@ -79,9 +88,79 @@ let f_not _ (args: Args) : JsonNode =
 
 // Conversion functions
 
+let f_base64 _ (args: Args) : JsonNode =
+    expectArgs 1 args
+    let str = ensureString <| List.head args
+
+    JsonValue.Create(toBase64 str)
+
+let f_base64ToBinary _ (args: Args) : JsonNode =
+    expectArgs 1 args
+    let str = ensureString <| List.head args
+
+    str |> fromBase64 |> toBinary
+
+let f_base64ToString _ (args: Args) : JsonNode =
+    expectArgs 1 args
+    let str = ensureString <| List.head args
+
+    str |> fromBase64 |> (fun v -> JsonValue.Create(v))
+
+let f_binary _ (args: Args) : JsonNode =
+    expectArgs 1 args
+    let str = ensureString <| List.head args
+
+    toBinary str
+
+let f_decimal _ (args: Args) : JsonNode =
+    expectArgs 1 args
+    let str = objectToString <| List.head args
+
+    // TODO: verify how it parses
+    // TODO: do arithmetic on decimals?
+    match
+        System.Decimal.TryParse(
+            str,
+            System.Globalization.NumberStyles.Any,
+            System.Globalization.CultureInfo.InvariantCulture
+        )
+    with
+    | true, result -> JsonValue.Create(result)
+    | _ -> failwithf "Could not parse %s as decimal" str
+
+let f_json _ (args: Args) : JsonNode =
+    expectArgs 1 args
+    let str = ensureString <| List.head args
+
+    JsonNode.Parse(str)
+
 let f_string _ (args: Args) : JsonNode =
     expectArgs 1 args
     JsonValue.Create(args |> List.head |> objectToString)
+
+// Math functions
+
+let f_sub _ (args: Args) : JsonNode =
+    match args with
+    | [ a; b ] ->
+        match a.GetValueKind(), b.GetValueKind() with
+        | JsonValueKind.Number, JsonValueKind.Number ->
+            let a = box <| a.GetValue()
+            let b = box <| b.GetValue()
+
+            match a, b with
+            | :? int as a, (:? int as b) -> JsonValue.Create(a - b)
+            | :? float as a, (:? float as b) -> JsonValue.Create(a - b)
+            | :? int as a, (:? float as b) -> JsonValue.Create(float a - b)
+            | :? float as a, (:? int as b) -> JsonValue.Create(a - float b)
+            | :? decimal as a, (:? decimal as b) -> JsonValue.Create(a - b)
+            | :? decimal as a, (:? int as b) -> JsonValue.Create(a - decimal b)
+            | :? int as a, (:? decimal as b) -> JsonValue.Create(decimal a - b)
+            | :? float as a, (:? decimal as b) -> JsonValue.Create(decimal a - b)
+            | :? decimal as a, (:? float as b) -> JsonValue.Create(a - decimal b)
+            | _ -> failwithf "Expected numbers, got %A and %A" (a.GetType()) (b.GetType())
+        | kindA, kindB -> failwithf "Expected numbers, got %A and %A" kindA kindB
+    | _ -> failwith "Expected 2 arguments"
 
 // Workflow functions
 
@@ -121,7 +200,14 @@ let functions: Map<string, LanguageFunction> =
       "startsWith", f_startsWith
       "item", f_item
       "not", f_not
+      "base64", f_base64
+      "base64ToBinary", f_base64ToBinary
+      "base64ToString", f_base64ToString
+      "binary", f_binary
+      "decimal", f_decimal
+      "json", f_json
       "string", f_string
+      "sub", f_sub
       "outputs", f_outputs
       "trigger", f_trigger
       "variables", f_variables ]
