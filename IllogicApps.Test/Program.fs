b89b785d58b6ac01a1f6fd5a78e4b47dcd1fe0c9
Score_Under <seejay.11@gmail.com>
2024-10-22 18:49:20 +0100

    Update example workflows etc

--- IllogicApps.Test/Program.fs
+++ IllogicApps.Test/Program.fs
@@ -4,16 +4,35 @@ open System.Text.Json.Nodes
 open ReadLogicApp
 open IllogicApps.Simulator
 
-let examplePath = "/home/score/src/IllogicApps/Stateful1/workflow.json"
+let examplePath = "Stateful1/workflow.json"
 
-let logicApp = readLogicApp examplePath
+// let logicApp = readLogicApp examplePath
+// let logicApp = readLogicApp <| examplePath.Replace("1", "2")
+let logicApp = readLogicApp <| examplePath.Replace("Stateful1", "SkippingTest")
+// let logicApp = readLogicApp <| examplePath.Replace("Stateful1", "FailurePropagationTest")
 
-printfn "%A" logicApp
+// printfn "%A" logicApp
 
-logicApp.definition.actions
-|> LogicAppActionSupport.fromKvps
-|> Seq.toList
-|> LogicAppBaseAction.getAllChildren
-|> printfn "%A"
+// logicApp.definition.actions
+// |> LogicAppActionSupport.fromKvps
+// |> Seq.toList
+// |> LogicAppBaseAction.getAllChildren
+// |> printfn "%A"
 
-Simulator.Trigger logicApp (JsonValue.Create("FAKE INPUT")) |> ignore
+let sim = Simulator.Trigger logicApp (JsonValue.Create("FAKE INPUT"))
+
+let opts = makeJsonSerializerOptions()
+opts.WriteIndented <- true
+
+printfn "\nAction results:\n---------------\n"
+sim.ActionResults
+|> fun c -> System.Text.Json.JsonSerializer.Serialize(c, opts)
+|> printfn "%s"
+
+printfn "\nVariable results:\n-----------------\n"
+sim.Variables
+|> fun c -> System.Text.Json.JsonSerializer.Serialize(c, opts)
+|> printfn "%s"
+
+if sim.LoopContextStack.Count <> 0 then failwith "Loop context stack not empty"
+if sim.ArrayOperationContextStack.Count <> 0 then failwith "Scope context stack not empty"
