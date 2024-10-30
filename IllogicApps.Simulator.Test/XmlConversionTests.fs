0def61ceffa89cfcda3a2b9b6b1a49d6db06e8f1
Score_Under <seejay.11@gmail.com>
2024-10-30 08:57:10 +0000

    More XML test cases

--- IllogicApps.Simulator.Test/XmlConversionTests.fs
+++ IllogicApps.Simulator.Test/XmlConversionTests.fs
@@ -36,10 +36,30 @@ let JsonOfXmlTest expr (expected: string) =
     let expected = JsonSerializer.Deserialize<JsonNode>(expected)
     test <@ jsonsEqual expected (testExpressionEvaluation expr) @>
 
-[<TestCase("@{xml('<root>a<!--b-->c<!--d--><e><![CDATA[Testing]]><!-- one big comment --></e>f</root>')}",
-           "<root>a<!--b-->c<!--d--><e><![CDATA[Testing]]><!-- one big comment --></e>f</root>")>]
+[<TestCase("@{xml('<root/>')}", "<root />")>]
 [<TestCase("@{xml('<root><node1 attr=\"x\"/><node2 attr2=\"y\"><innermost innerattr=\"o&quot;h\">wow</innermost></node2></root>')}",
            "<root><node1 attr=\"x\" /><node2 attr2=\"y\"><innermost innerattr=\"o&quot;h\">wow</innermost></node2></root>")>]
+[<TestCase("@{xml('<root><node1 attr=\"x\"/><node2 attr2=\"y\"><innermost innerattr=\"o&quot;h\">wow<break/>wowow</innermost></node2></root>')}",
+           "<root><node1 attr=\"x\" /><node2 attr2=\"y\"><innermost innerattr=\"o&quot;h\">wow<break />wowow</innermost></node2></root>")>]
+[<TestCase("@{xml('<root>a<!--b-->c<!--d--><e><![CDATA[Testing]]><!-- one big comment --></e>f</root>')}",
+           "<root>a<!--b-->c<!--d--><e><![CDATA[Testing]]><!-- one big comment --></e>f</root>")>]
+[<TestCase("@{xml('<root>aaa<!-- one big comment --><e><![CDATA[Testing]]></e>f</root>')}",
+           "<root>aaa<!-- one big comment --><e><![CDATA[Testing]]></e>f</root>")>]
+[<TestCase("@{xml('<root>aaa<e><![CDATA[Testing]]><!-- one big comment --></e>f</root>')}",
+           "<root>aaa<e><![CDATA[Testing]]><!-- one big comment --></e>f</root>")>]
+[<TestCase("@{xml('<root>aaa<e><![CDATA[Testing]]></e>f<!-- one big comment --></root>')}",
+           "<root>aaa<e><![CDATA[Testing]]></e>f<!-- one big comment --></root>")>]
+[<TestCase("@{xml('<root>ac<!--big comment--><e><!-- one big comment --></e>f</root>')}",
+           "<root>ac<!--big comment--><e><!-- one big comment --></e>f</root>")>]
+[<TestCase("@{xml('<?xml version=\"1.0\" encoding=\"utf-8\"?><root/>')}",
+           "<?xml version=\"1.0\" encoding=\"utf-8\"?><root />")>]
+[<TestCase("@{xml('<root><!-- comment --></root>')}", "<root><!-- comment --></root>")>]
+[<TestCase("@{xml('<root attr=\"val\"/>')}", "<root attr=\"val\" />")>]
+[<TestCase("@{xml('<root> a <!-- b --> c </root>')}", "<root> a <!-- b --> c </root>")>]
+[<TestCase("@{xml('<root> <!-- a --> b <!-- c --> </root>')}", "<root><!-- a --> b <!-- c --></root>")>]
+[<TestCase("@{xml('<root><![CDATA[<>&''\"]]></root>')}", "<root><![CDATA[<>&'\"]]></root>")>]
+[<TestCase("@{xml('<root><![CDATA[<!--]]>testing<![CDATA[-->]]></root>')}",
+           "<root><![CDATA[<!--]]>testing<![CDATA[-->]]></root>")>]
 let StringifiedXmlTest expr expected =
     test <@ expected.Equals(jsonToObject (testExpressionEvaluation expr)) @>
 
@@ -66,6 +86,33 @@ let ObjectOfXmlTest expr (expected: string) =
 let XmlJsonRoundTrippingTest expr (expected: string) =
     let expected = JsonSerializer.Deserialize<JsonNode>(expected)
     test <@ jsonsEqual expected (testExpressionEvaluation expr) @>
+
+[<TestCase("@{xml(json(xml('<root/>')))}", "<root />")>]
+[<TestCase("@{xml(json(xml('<root><node1 attr=\"x\"/><node2 attr2=\"y\"><innermost innerattr=\"o&quot;h\">wow</innermost></node2></root>')))}",
+           "<root><node1 attr=\"x\" /><node2 attr2=\"y\"><innermost innerattr=\"o&quot;h\">wow</innermost></node2></root>")>]
+[<TestCase("@{xml(json(xml('<root><node1 attr=\"x\"/><node2 attr2=\"y\"><innermost innerattr=\"o&quot;h\">wow<break/>wowow</innermost></node2></root>')))}",
+           "<root><node1 attr=\"x\" /><node2 attr2=\"y\"><innermost innerattr=\"o&quot;h\">wowwowow<break /></innermost></node2></root>")>]
+[<TestCase("@{xml(json(xml('<root>a<!--b-->c<!--d--><e><![CDATA[Testing]]><!-- one big comment --></e>f</root>')))}",
+           "<root>acf<e><![CDATA[Testing]]></e></root>")>]
+[<TestCase("@{xml(json(xml('<root>aaa<!-- one big comment --><e><![CDATA[Testing]]></e>f</root>')))}",
+           "<root>aaaf<e><![CDATA[Testing]]></e></root>")>]
+[<TestCase("@{xml(json(xml('<root>aaa<e><![CDATA[Testing]]><!-- one big comment --></e>f</root>')))}",
+           "<root>aaaf<e><![CDATA[Testing]]></e></root>")>]
+[<TestCase("@{xml(json(xml('<root>aaa<e><![CDATA[Testing]]></e>f<!-- one big comment --></root>')))}",
+           "<root>aaaf<e><![CDATA[Testing]]></e></root>")>]
+[<TestCase("@{xml(json(xml('<root>ac<!--big comment--><e><!-- one big comment --></e>f</root>')))}",
+           "<root>acf<e /></root>")>]
+[<TestCase("@{xml(json(xml('<?xml version=\"1.0\" encoding=\"utf-8\"?><root/>')))}",
+           "<?xml version=\"1.0\" encoding=\"utf-8\"?><root />")>]
+[<TestCase("@{xml(json(xml('<root><!-- comment --></root>')))}", "<root />")>]
+[<TestCase("@{xml(json(xml('<root attr=\"val\"/>')))}", "<root attr=\"val\" />")>]
+[<TestCase("@{xml(json(xml('<root> a <!-- b --> c </root>')))}", "<root> a  c </root>")>]
+[<TestCase("@{xml(json(xml('<root> <!-- a --> b <!-- c --> </root>')))}", "<root> b </root>")>]
+[<TestCase("@{xml(json(xml('<root><![CDATA[<>&''\"]]></root>')))}", "<root><![CDATA[<>&'\"]]></root>")>]
+[<TestCase("@{xml(json(xml('<root><![CDATA[<!--]]>testing<![CDATA[-->]]></root>')))}",
+           "<root><![CDATA[<!--]]><![CDATA[-->]]>testing</root>")>]
+let StringifiedXmlJsonRoundTrippingTest expr expected =
+    test <@ expected.Equals(jsonToObject (testExpressionEvaluation expr)) @>
 
 [<Test>]
 let XmlOfXmlTest () =
@@ -112,4 +159,10 @@ let JsonToXmlEmptyObjectIsEmptyDocumentTest () =
 [<TestCase("""@json(xml(json('{}')))""")>]
 [<TestCase("""@json(xml(json('{"hello":"world","oh no":"another root"}')))""")>]
 let JsonInvalidXmlRoundTripTest expr =
+    raises<XmlException> <@ testExpressionEvaluation expr @>
+
+[<TestCase("@{xml('<?xml version=\"1.0\" encoding=\"utf-8\"?>')}")>]
+[<TestCase("@{xml('<!-- comment -->')}")>]
+[<TestCase("@{xml('<root> a <!-- b <!-- c --> d --> e </root>')}")>]
+let InvalidXmlTest expr =
     raises<XmlException> <@ testExpressionEvaluation expr @>
