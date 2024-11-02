7dca6e51703bde5a93e52c70d1ed526417d81248
Score_Under <seejay.11@gmail.com>
2024-11-02 23:21:39 +0000

    Formatting annoyance

--- IllogicApps.Json/Parser.fs
+++ IllogicApps.Json/Parser.fs
@@ -294,12 +294,7 @@ let parse (str: string) =
                 | '\t'
                 | '\n'
                 | '\r' -> parse' (index + 1) ObjectStart stack
-                | '"' ->
-                    parse'
-                        (index + 1)
-
-                        (StringLiteral(StringBuilder()))
-                        (ConstructingObject [] :: stack)
+                | '"' -> parse' (index + 1) (StringLiteral(StringBuilder())) (ConstructingObject [] :: stack)
                 | '}' -> parse' (index + 1) (ValueEnd(JsonTree.Object(Map.empty))) stack
                 | c -> fail c index state stack
             | ArrayStart ->
