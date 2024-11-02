ad2106fa8679ff8036b0f8ab34177e793066d2a6
Score_Under <seejay.11@gmail.com>
2024-11-02 10:08:25 +0000

    Working on json parser performance

--- IllogicApps.Json/Parser.fs
+++ IllogicApps.Json/Parser.fs
@@ -1,5 +1,6 @@
 module IllogicApps.Json.Parser
 
+open System
 open System.Collections.Immutable
 open System.Globalization
 open System.Text
@@ -28,7 +29,7 @@ type private ConstructingState =
 
 let private parseFloat index (str: string) =
     match
-        System.Double.TryParse(
+        Double.TryParse(
             str,
             NumberStyles.AllowLeadingSign
             ||| NumberStyles.AllowDecimalPoint
@@ -40,28 +41,40 @@ let private parseFloat index (str: string) =
     | _ -> failwithf "Unexpected number format '%s' at index %d" str index
 
 let private parseInteger index (str: string) =
-    match System.Int64.TryParse(str, NumberStyles.AllowLeadingSign, CultureInfo.InvariantCulture) with
+    match Int64.TryParse(str, NumberStyles.AllowLeadingSign, CultureInfo.InvariantCulture) with
     | true, result -> Integer result
     | _ -> parseFloat index str
 
 type JsonFormatException(message) =
-    inherit System.FormatException(message)
+    inherit FormatException(message)
 
 let private fail (c: char) (index: int) (state: ParserState) (stack: ConstructingState list) =
     if index = -1 then
-        raise (JsonFormatException(sprintf "Unexpected end of input in state %A with stack %A" state stack))
+        match state, stack with
+        | StringLiteral _, _ -> "Unexpected end of input in string literal. Missing closing quote."
+        | _, ConstructingObjectValue _ :: _ -> "Unexpected end of input in object. Missing closing brace."
+        | _, ConstructingObject _ :: _ -> "Unexpected end of input in object. Missing value and closing brace."
+        | _, ConstructingArray _ :: _ -> "Unexpected end of input in array. Missing closing bracket."
+        | _ -> sprintf "Unexpected end of input in state %A with stack %A" state stack
+        |> JsonFormatException
+        |> raise
     else
-        raise (
-            JsonFormatException(
-                sprintf "Unexpected character '%c' at index %d in state %A with stack %A" c index state stack
-            )
-        )
+        match state, stack with
+        | ValueEnd _, ConstructingArray _ :: _ ->
+            sprintf "Unexpected character '%c' at index %d: Expecting comma or closing bracket." c index
+        | ValueEnd _, ConstructingObjectValue _ :: _ ->
+            sprintf "Unexpected character '%c' at index %d: Expecting comma or closing brace." c index
+        | ValueEnd _, ConstructingObject _ :: _ ->
+            sprintf "Unexpected character '%c' at index %d: Expecting colon." c index
+        | _ -> sprintf "Unexpected character '%c' at index %d in state %A with stack %A" c index state stack
+        |> JsonFormatException
+        |> raise
 
 let private parseLiteralIdentifier s c index state stack =
     match s with
     | "null" -> Null
-    | "true" -> Boolean true
-    | "false" -> Boolean false
+    | "true" -> JsonTree.Boolean true
+    | "false" -> JsonTree.Boolean false
     | _ -> fail c index state stack
 
 let parse (str: string) =
@@ -86,7 +99,7 @@ let parse (str: string) =
                 | '"' -> parse' (index + 1) (StringLiteral(StringBuilder())) stack
                 | '-' as c -> parse' (index + 1) (NumberZeroOrDigit(StringBuilder().Append(c))) stack
                 | '0' as c -> parse' (index + 1) (NumberFractionDot(StringBuilder().Append(c))) stack
-                | c when System.Char.IsAsciiDigit(c) ->
+                | c when Char.IsAsciiDigit(c) ->
                     parse' (index + 1) (NumberDigit(StringBuilder().Append(c))) stack
                 | '[' -> parse' (index + 1) ArrayStart stack
                 | '{' -> parse' (index + 1) ObjectStart stack
@@ -115,43 +128,71 @@ let parse (str: string) =
                 | ']' ->
                     match stack with
                     | ConstructingArray a :: stack' ->
-                        parse' (index + 1) (ValueEnd(Array(ImmutableArray.ToImmutableArray(List.rev (v :: a))))) stack'
+                        parse' (index + 1) (ValueEnd(JsonTree.Array(ImmutableArray.ToImmutableArray(List.rev (v :: a))))) stack'
                     | _ -> fail c index state stack
                 | '}' ->
                     match stack with
                     | ConstructingObjectValue(k, o) :: stack' ->
-                        parse' (index + 1) (ValueEnd(Object(Map.ofList ((k, v) :: o)))) stack'
+                        parse' (index + 1) (ValueEnd(JsonTree.Object(Map.ofList ((k, v) :: o)))) stack'
                     | _ -> fail c index state stack
                 | _ -> fail c index state stack
             | StringLiteral sb ->
-                match c with
-                | '\\' -> parse' (index + 1) (StringEscape sb) stack
-                | '"' -> parse' (index + 1) (ValueEnd(String(sb.ToString()))) stack
-                | c when c < char 0x20 -> fail c index state stack
-                | c -> parse' (index + 1) (StringLiteral(sb.Append(c))) stack
-            | StringEscape sb ->
-                match c with
-                | '"'
-                | '\\'
-                | '/' as c -> parse' (index + 1) (StringLiteral(sb.Append(c))) stack
-                | 'b' -> parse' (index + 1) (StringLiteral(sb.Append('\b'))) stack
-                | 'f' -> parse' (index + 1) (StringLiteral(sb.Append('\f'))) stack
-                | 'n' -> parse' (index + 1) (StringLiteral(sb.Append('\n'))) stack
-                | 'r' -> parse' (index + 1) (StringLiteral(sb.Append('\r'))) stack
-                | 't' -> parse' (index + 1) (StringLiteral(sb.Append('\t'))) stack
-                | 'u' when index + 4 < str.Length ->
-                    let hex = str.Substring(index + 1, 4)
-                    let c = char (System.Int32.Parse(hex, NumberStyles.HexNumber))
-                    parse' (index + 5) (StringLiteral(sb.Append(c))) stack
-                | c -> fail c index state stack
+                let rec auxParse start index =
+                    let finishStep () =
+                        sb.Append(str.AsSpan().Slice(start, index - start)) |> ignore
+
+                    match str.[index] with
+                    | '\\' ->
+                        finishStep ()
+                        auxEscape (index + 1)
+                    | '"' ->
+                        finishStep ()
+                        parse' (index + 1) (ValueEnd(JsonTree.String(sb.ToString()))) stack
+                    | c when c < char 0x20 ->
+                        finishStep ()
+                        fail c index state stack
+                    | _ -> auxParse start (index + 1)
+
+                and startAuxParse i = auxParse i i
+
+                and auxEscape index =
+                    match str.[index] with
+                    | '"'
+                    | '\\'
+                    | '/' as c ->
+                        sb.Append(c) |> ignore
+                        startAuxParse (index + 1)
+                    | 'b' ->
+                        sb.Append('\b') |> ignore
+                        startAuxParse (index + 1)
+                    | 'f' ->
+                        sb.Append('\f') |> ignore
+                        startAuxParse (index + 1)
+                    | 'n' ->
+                        sb.Append('\n') |> ignore
+                        startAuxParse (index + 1)
+                    | 'r' ->
+                        sb.Append('\r') |> ignore
+                        startAuxParse (index + 1)
+                    | 't' ->
+                        sb.Append('\t') |> ignore
+                        startAuxParse (index + 1)
+                    | 'u' when index + 4 < str.Length ->
+                        let hex = str.Substring(index + 1, 4)
+                        let c = char (Int32.Parse(hex, NumberStyles.HexNumber))
+                        sb.Append(c) |> ignore
+                        startAuxParse (index + 5)
+                    | c -> fail c index (StringEscape sb) stack
+
+                startAuxParse index
             | NumberZeroOrDigit sb ->
                 match c with
                 | '0' as c -> parse' (index + 1) (NumberFractionDot(sb.Append(c))) stack
-                | c when System.Char.IsAsciiDigit(c) -> parse' (index + 1) (NumberDigit(sb.Append(c))) stack
+                | c when Char.IsAsciiDigit(c) -> parse' (index + 1) (NumberDigit(sb.Append(c))) stack
                 | _ -> fail c index state stack
             | NumberDigit sb ->
                 match c with
-                | c when System.Char.IsAsciiDigit(c) -> parse' (index + 1) (NumberDigit(sb.Append(c))) stack
+                | c when Char.IsAsciiDigit(c) -> parse' (index + 1) (NumberDigit(sb.Append(c))) stack
                 | '.' as c -> parse' (index + 1) (NumberFractionStart(sb.Append(c))) stack
                 | 'e'
                 | 'E' as c -> parse' (index + 1) (NumberExponentSign(sb.Append(c))) stack
@@ -162,11 +203,11 @@ let parse (str: string) =
                 | _ -> parse' index (ValueEnd(parseInteger index (sb.ToString()))) stack
             | NumberFractionStart sb ->
                 match c with
-                | c when System.Char.IsAsciiDigit(c) -> parse' (index + 1) (NumberFraction(sb.Append(c))) stack
+                | c when Char.IsAsciiDigit(c) -> parse' (index + 1) (NumberFraction(sb.Append(c))) stack
                 | _ -> fail c index state stack
             | NumberFraction sb ->
                 match c with
-                | c when System.Char.IsAsciiDigit(c) -> parse' (index + 1) (NumberFraction(sb.Append(c))) stack
+                | c when Char.IsAsciiDigit(c) -> parse' (index + 1) (NumberFraction(sb.Append(c))) stack
                 | 'e'
                 | 'E' as c -> parse' (index + 1) (NumberExponentSign(sb.Append(c))) stack
                 | _ -> parse' index (ValueEnd(parseFloat index (sb.ToString()))) stack
@@ -174,15 +215,15 @@ let parse (str: string) =
                 match c with
                 | '+'
                 | '-' as c -> parse' (index + 1) (NumberExponentDigitStart(sb.Append(c))) stack
-                | c when System.Char.IsAsciiDigit(c) -> parse' (index + 1) (NumberExponentDigit(sb.Append(c))) stack
+                | c when Char.IsAsciiDigit(c) -> parse' (index + 1) (NumberExponentDigit(sb.Append(c))) stack
                 | _ -> fail c index state stack
             | NumberExponentDigitStart sb ->
                 match c with
-                | c when System.Char.IsAsciiDigit(c) -> parse' (index + 1) (NumberExponentDigit(sb.Append(c))) stack
+                | c when Char.IsAsciiDigit(c) -> parse' (index + 1) (NumberExponentDigit(sb.Append(c))) stack
                 | _ -> fail c index state stack
             | NumberExponentDigit sb ->
                 match c with
-                | c when System.Char.IsAsciiDigit(c) -> parse' (index + 1) (NumberExponentDigit(sb.Append(c))) stack
+                | c when Char.IsAsciiDigit(c) -> parse' (index + 1) (NumberExponentDigit(sb.Append(c))) stack
                 | _ -> parse' index (ValueEnd(parseFloat index (sb.ToString()))) stack
             | ObjectStart ->
                 match c with
@@ -196,7 +237,7 @@ let parse (str: string) =
 
                         (StringLiteral(StringBuilder()))
                         (ConstructingObject [] :: stack)
-                | '}' -> parse' (index + 1) (ValueEnd(Object(Map.empty))) stack
+                | '}' -> parse' (index + 1) (ValueEnd(JsonTree.Object(Map.empty))) stack
                 | _ -> fail c index state stack
             | ArrayStart ->
                 match c with
@@ -204,11 +245,11 @@ let parse (str: string) =
                 | '\t'
                 | '\n'
                 | '\r' -> parse' (index + 1) ArrayStart stack
-                | ']' -> parse' (index + 1) (ValueEnd(Array(ImmutableArray.Empty))) stack
+                | ']' -> parse' (index + 1) (ValueEnd(JsonTree.Array(ImmutableArray.Empty))) stack
                 | _ -> parse' index ValueStart (ConstructingArray [] :: stack)
             | LiteralIdentifier sb ->
                 match c with
-                | c when System.Char.IsAsciiLetter(c) -> parse' (index + 1) (LiteralIdentifier(sb.Append(c))) stack
+                | c when Char.IsAsciiLetter(c) -> parse' (index + 1) (LiteralIdentifier(sb.Append(c))) stack
                 | _ -> parse' index (ValueEnd(parseLiteralIdentifier (sb.ToString()) c index state stack)) stack
 
     parse' 0 ValueStart []
