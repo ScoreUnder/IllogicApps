655c9e2d1fe727bb03ee039c23a2945abed74cd9
Score_Under <seejay.11@gmail.com>
2024-11-02 09:13:02 +0000

    Convenience functions for JSON access

--- IllogicApps.Json/CoreTypes.fs
+++ IllogicApps.Json/CoreTypes.fs
@@ -12,3 +12,28 @@ type JsonTree =
     | Float of float
     | Decimal of decimal
     | Boolean of bool
+
+module JsonTree =
+    let tryGetKey (key: string) (json: JsonTree) =
+        match json with
+        | Object o -> o.TryFind key
+        | _ -> None
+
+    let tryGetIndex (index: int) (json: JsonTree) =
+        match json with
+        | Array a ->
+            if index >= 0 && index < a.Length then
+                Some a.[index]
+            else
+                None
+        | _ -> None
+
+    let getKey (key: string) (json: JsonTree) =
+        match json with
+        | Object o -> o.[key]
+        | _ -> failwithf "Expected object, got %A" json
+
+    let getIndex (index: int) (json: JsonTree) =
+        match json with
+        | Array a -> a.[index]
+        | _ -> failwithf "Expected array, got %A" json
