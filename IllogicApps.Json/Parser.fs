351cafd6614221436d95304bc6ee1d9fae2f5868
Score_Under <seejay.11@gmail.com>
2024-11-03 10:54:15 +0000

    Tidy vector stuff a little

--- IllogicApps.Json/Parser.fs
+++ IllogicApps.Json/Parser.fs
@@ -82,20 +82,11 @@ let private parseLiteralIdentifier s c index state stack =
 
 #nowarn "9" // FS0009: Uses of this construct may result in the generation of unverifiable .NET IL code.
 
-let private getNumStringPassthroughChars (str: char ReadOnlySpan) =
-    // let charVec = Vector256.Create<int16>(MemoryMarshal.Cast<char, int16>(str))
-
-    // Avoid size-of-span integer overflow check which you get with MemoryMarshal.Cast (we do not use the full span)
+let inline private unsafeGetVectorOfString (str: char ReadOnlySpan) =
     use addr = fixed &MemoryMarshal.GetReference(str)
+    Unsafe.ReadUnaligned<Vector256<int16>>(FSharp.NativeInterop.NativePtr.toVoidPtr addr)
 
-    let charVec =
-        Unsafe.ReadUnaligned<Vector256<int16>>(FSharp.NativeInterop.NativePtr.toVoidPtr addr)
-
-    let matchesVector =
-        Vector256.Equals(charVec, Vector256.Create(int16 '"'))
-        |> (fun f -> Vector256.BitwiseOr(f, Vector256.Equals(charVec, Vector256.Create(int16 '\\'))))
-        |> (fun f -> Vector256.BitwiseOr(f, Vector256.LessThan(charVec, Vector256.Create(int16 ' '))))
-
+let inline private countMatches matchesVector =
     if X86.Avx2.IsSupported then
         matchesVector
         |> Vector256.AsByte
@@ -111,38 +102,24 @@ let private getNumStringPassthroughChars (str: char ReadOnlySpan) =
         |> Vector256.Sum
         |> Int16.TrailingZeroCount
         |> int32
+
+let private getNumStringPassthroughChars (str: char ReadOnlySpan) =
+    let charVec = unsafeGetVectorOfString str
+
+    Vector256.Equals(charVec, Vector256.Create(int16 '"'))
+    |> (fun f -> Vector256.BitwiseOr(f, Vector256.Equals(charVec, Vector256.Create(int16 '\\'))))
+    |> (fun f -> Vector256.BitwiseOr(f, Vector256.LessThan(charVec, Vector256.Create(int16 ' '))))
+    |> countMatches
 
 let private getNumWhitespaces (str: char ReadOnlySpan) =
-    // let charVec = Vector256.Create<int16>(MemoryMarshal.Cast<char, int16>(str))
+    let charVec = unsafeGetVectorOfString str
 
-    // Avoid size-of-span integer overflow check which you get with MemoryMarshal.Cast (we do not use the full span)
-    use addr = fixed &MemoryMarshal.GetReference(str)
-
-    let charVec =
-        Unsafe.ReadUnaligned<Vector256<int16>>(FSharp.NativeInterop.NativePtr.toVoidPtr addr)
-
-    let matchesVector =
-        Vector256.Equals(charVec, Vector256.Create(int16 ' '))
-        |> (fun f -> Vector256.BitwiseOr(f, Vector256.Equals(charVec, Vector256.Create(int16 '\t'))))
-        |> (fun f -> Vector256.BitwiseOr(f, Vector256.Equals(charVec, Vector256.Create(int16 '\n'))))
-        |> (fun f -> Vector256.BitwiseOr(f, Vector256.Equals(charVec, Vector256.Create(int16 '\r'))))
-        |> Vector256.OnesComplement
-
-    if X86.Avx2.IsSupported then
-        matchesVector
-        |> Vector256.AsByte
-        |> X86.Avx2.MoveMask
-        |> Int32.TrailingZeroCount
-        |> fun v -> v >>> 1
-    else
-        let mask = Vector256.Create(
-            0x0001s, 0x0002s, 0x0004s, 0x0008s, 0x0010s, 0x0020s, 0x0040s, 0x0080s,
-            0x0100s, 0x0200s, 0x0400s, 0x0800s, 0x1000s, 0x2000s, 0x4000s, 0x8000s)
-
-        Vector256.BitwiseAnd(mask, matchesVector)
-        |> Vector256.Sum
-        |> Int16.TrailingZeroCount
-        |> int32
+    Vector256.Equals(charVec, Vector256.Create(int16 ' '))
+    |> (fun f -> Vector256.BitwiseOr(f, Vector256.Equals(charVec, Vector256.Create(int16 '\t'))))
+    |> (fun f -> Vector256.BitwiseOr(f, Vector256.Equals(charVec, Vector256.Create(int16 '\n'))))
+    |> (fun f -> Vector256.BitwiseOr(f, Vector256.Equals(charVec, Vector256.Create(int16 '\r'))))
+    |> Vector256.OnesComplement
+    |> countMatches
 
 let parse (str: string) =
     let rec parse' (index: int) (state: ParserState) (stack: ConstructingState list) =
