45c79f95473bd7a13a14b241b96421a48f9c7263
Score_Under <seejay.11@gmail.com>
2024-11-25 21:00:09 +0000

    Update flake lock; temporary use of nixpkgs fork for rider

--- flake.nix
+++ flake.nix
@@ -2,7 +2,8 @@
   description = "Dev flake for IllogicApps";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    #nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:archevel/nixpkgs?ref=patch-1";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
