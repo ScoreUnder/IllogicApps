c1fb0e50791e696400762fd760a9602af6da07d7
Score_Under <seejay.11@gmail.com>
2024-12-05 11:40:39 +0000

    Add test for newtonsoft JSON conversion round trip

---
+++ IllogicApps.Compat.LogicAppUnit.Test/NewtonsoftJsonConversions.fs
@@ -0,0 +1,28 @@
+module IllogicApps.Compat.LogicAppUnit.Test
+
+open NUnit.Framework
+open Newtonsoft.Json.Linq
+open Swensen.Unquote
+
+open IllogicApps.Json
+open NewtonsoftJsonConversions
+
+let ``JSON round-trip test cases`` =
+    [ """{"null": null, "int": 123, "float": 123.0, "bool": false, "array": [1,2,3], "object": {}}""" ]
+    |> List.map TestCaseData
+
+[<TestCaseSource(nameof ``JSON round-trip test cases``)>]
+let ``Test Illogic-to-Newtonsoft conversion`` expr =
+    let illogic = Parser.parse expr
+    let newtonsoft = newtonsoftJsonOfIllogicJson illogic
+    let newtonsoftString = newtonsoft.ToString()
+    let illogicOfString = Parser.parse newtonsoftString
+    test <@ illogicOfString = illogic @>
+
+[<TestCaseSource(nameof ``JSON round-trip test cases``)>]
+let ``Test Newtonsoft-to-Illogic conversion`` expr =
+    let newtonsoft = JToken.Parse expr
+    let illogic = illogicJsonOfNewtonsoftJson newtonsoft
+    let illogicString = illogic.ToString()
+    let newtonsoftOfString = JToken.Parse illogicString
+    test <@ JToken.DeepEquals(newtonsoftOfString, newtonsoft) @>
