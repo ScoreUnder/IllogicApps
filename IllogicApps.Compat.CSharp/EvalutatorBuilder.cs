c7a5251f699c4e70c5dfee5214768362abced21f
Score_Under <seejay.11@gmail.com>
2025-04-25 09:48:18 +0100

    Missed in namespace refactor...

--- IllogicApps.Compat.CSharp/EvalutatorBuilder.cs
+++ IllogicApps.Compat.CSharp/EvalutatorBuilder.cs
@@ -9,12 +9,11 @@ namespace IllogicApps.Compat.CSharp;
 
 public class EvalutatorBuilder
 {
-    private OrderedMap.Builder<string,
-            FSharpFunc<SimulatorContext, FSharpFunc<FSharpList<CoreTypes.JsonTree>, CoreTypes.JsonTree>>>
+    private OrderedMap.Builder<string, FSharpFunc<SimulatorContext, FSharpFunc<FSharpList<JsonTree>, JsonTree>>>
         _functions = new();
 
-    private OrderedMap.Builder<string, FSharpFunc<SimulatorContext,
-        FSharpFunc<FSharpList<Lazy<CoreTypes.JsonTree>>, CoreTypes.JsonTree>>> _lazyFunctions = new();
+    private OrderedMap.Builder<string, FSharpFunc<SimulatorContext, FSharpFunc<FSharpList<Lazy<JsonTree>>, JsonTree>>>
+        _lazyFunctions = new();
 
     private EvalutatorBuilder()
     {
@@ -31,30 +30,28 @@ public class EvalutatorBuilder
     }
 
     public EvalutatorBuilder SetFunction(string name,
-        Func<SimulatorContext, FSharpList<CoreTypes.JsonTree>, CoreTypes.JsonTree> functionToAdd)
+        Func<SimulatorContext, FSharpList<JsonTree>, JsonTree> functionToAdd)
     {
         _functions.Set(name, FuncConvert.FromFunc(functionToAdd));
         return this;
     }
 
     public EvalutatorBuilder SetLazyFunction(string name,
-        Func<SimulatorContext, FSharpList<Lazy<CoreTypes.JsonTree>>, CoreTypes.JsonTree> functionToAdd)
+        Func<SimulatorContext, FSharpList<Lazy<JsonTree>>, JsonTree> functionToAdd)
     {
         _lazyFunctions.Set(name, FuncConvert.FromFunc(functionToAdd));
         return this;
     }
 
     class BuiltResult(
-        OrderedMap<string, FSharpFunc<SimulatorContext, FSharpFunc<FSharpList<CoreTypes.JsonTree>, CoreTypes.JsonTree>>>
-            functions,
-        OrderedMap<string, FSharpFunc<SimulatorContext,
-            FSharpFunc<FSharpList<Lazy<CoreTypes.JsonTree>>, CoreTypes.JsonTree>>> lazyFunctions
-    ) : OptimizedClosures.FSharpFunc<SimulatorContext, Parser.Ast, CoreTypes.JsonTree>
+        OrderedMap<string, FSharpFunc<SimulatorContext, FSharpFunc<FSharpList<JsonTree>, JsonTree>>> functions,
+        OrderedMap<string, FSharpFunc<SimulatorContext, FSharpFunc<FSharpList<Lazy<JsonTree>>, JsonTree>>>
+            lazyFunctions) : OptimizedClosures.FSharpFunc<SimulatorContext, Parser.Ast, JsonTree>
     {
-        public override CoreTypes.JsonTree Invoke(SimulatorContext simContext, Parser.Ast ast) =>
+        public override JsonTree Invoke(SimulatorContext simContext, Parser.Ast ast) =>
             Evaluator.evaluateSandboxed(functions, lazyFunctions, simContext, ast);
     }
 
-    public FSharpFunc<SimulatorContext, FSharpFunc<Parser.Ast, CoreTypes.JsonTree>> Build() =>
+    public FSharpFunc<SimulatorContext, FSharpFunc<Parser.Ast, JsonTree>> Build() =>
         new BuiltResult(_functions.Build(), _lazyFunctions.Build());
 }
